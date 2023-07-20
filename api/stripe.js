ff75a65a31cde11520929d8dc1906429af04dd58
Ethan Ye <chiguayeshao@gmail.com>
2023-07-20 18:37:34 +0800

    Update stripe.js

--- api/stripe.js
+++ api/stripe.js
@@ -12,8 +12,6 @@ const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY, {
   apiVersion: "2022-08-01",
 });
 
-app.use(express.static(process.env.STATIC_DIR));
-
 app.post("/create-personal-checkout", async (req, res) => {
   const { ethAddress } = req.body;
   console.log(ethAddress, "ethAddress");
