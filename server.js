38e689ddc278e20bb71d5a06d0b246694535bc96
Ethan Ye <chiguayeshao@gmail.com>
2023-07-20 17:34:15 +0800

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,8 @@ const cors = require("cors");
 
 app.use(cors());
 
-// Replace if using a different env file or config
+app.use(express.json());
+
 const env = require("dotenv").config({ path: "./.env" });
 
 const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY, {
@@ -14,6 +15,9 @@ const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY, {
 app.use(express.static(process.env.STATIC_DIR));
 
 app.post("/create-personal-checkout", async (req, res) => {
+  const { ethAddress } = req.body;
+  console.log(ethAddress, "ethAddress");
+
   const session = await stripe.checkout.sessions.create({
     payment_method_types: ["card"],
     line_items: [
@@ -38,14 +42,19 @@ app.post("/create-personal-checkout", async (req, res) => {
       },
     ],
     mode: "payment",
-    success_url: "http://localhost:3000/GasSubscribe",
-    cancel_url: "http://localhost:3000/GasSubscribe",
+    success_url: "https://gaslockr-website.vercel.app",
+    cancel_url: "https://gaslockr-website.vercel.app",
+    metadata: {
+      ethAddress,
+    },
   });
 
   res.json({ id: session.id });
 });
 
 app.post("/create-professional-checkout", async (req, res) => {
+  const { ethAddress } = req.body;
+
   const session = await stripe.checkout.sessions.create({
     payment_method_types: ["card"],
     line_items: [
@@ -56,7 +65,7 @@ app.post("/create-professional-checkout", async (req, res) => {
             name: "GasLockR Professional Plan",
             description: "GasLockR Professional Plan",
             images: [
-              "https://stripe-camo.global.ssl.fastly.net/ae797d80f64f34c63a86ed5d85ee869bc8f72a9e9bdd684cae40d76e2b3ae374/68747470733a2f2f66696c65732e7374726970652e636f6d2f66696c65732f4d44423859574e6a64463878546c4e6864466c435957464255304e7a64574e7966475a6662476c325a56396165577069655670305a6c64334e457456546c5a3363455a465a6c413353453430304e76667a68593652",
+              "https://stripe-camo.global.ssl.fastly.net/ae797d80f64f34c63a86ed5d85ee869bc8f72a9e9bdd684cae40d76e2b3ae374/68747470733a2f2f66696c65732e7374726970652e636f6d2f66696c65732f4d44423859574e6a64463878546l4e6864466c435957464255304e7a64574e7966475a6662476c325a56396165577069655670305a6c64334e457456546c5a3363455a465a6c413353453430304e76667a68593652",
             ],
           },
           unit_amount: 2500,
@@ -70,10 +79,12 @@ app.post("/create-professional-checkout", async (req, res) => {
       },
     ],
     mode: "payment",
-    success_url: "http://localhost:3000/GasSubscribe",
-    cancel_url: "http://localhost:3000/GasSubscribe",
+    success_url: "https://gaslockr-website.vercel.app",
+    cancel_url: "https://gaslockr-website.vercel.app",
+    metadata: {
+      ethAddress,
+    },
   });
-
   res.json({ id: session.id });
 });
 
